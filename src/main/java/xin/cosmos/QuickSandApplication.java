d8ebf6cc248b45b9fff19803fe46be062f25e222
gengzhy <gengzyy@gmail.com>
2022-05-18 00:37:11 +0800

    测试

--- src/main/java/xin/cosmos/QuickSandApplication.java
+++ src/main/java/xin/cosmos/QuickSandApplication.java
@@ -1,10 +1,14 @@
 package xin.cosmos;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -19,10 +23,20 @@ public class QuickSandApplication {
     class DemoController {
 
         @ApiOperation(value = "hello 测试接口")
-        @GetMapping("/hello")
-        public String hello() {
-            return "hello world";
+        @PostMapping("/hello")
+        public User hello(@RequestBody User user) {
+            return user;
         }
+    }
+
+    @Data
+    @ApiModel(description = "用户参数")
+    static class User {
+        @ApiModelProperty(value = "用户名", required = true, example = "ian")
+        private String username;
+
+        @ApiModelProperty(value = "年龄", required = true, allowableValues = "[1,100]")
+        private Integer age;
     }
 
 }
